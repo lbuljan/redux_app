d0825e21c73bc5df1f355cc3a7d41ecc65ee40c3
Luka Buljan <luka@prototyp.digital>
2021-05-08 13:05:30 +0200

    prettier changes

--- src/modules/redux-store/factories/index.ts
+++ src/modules/redux-store/factories/index.ts
@@ -1,2 +1,2 @@
-export * from "./configureStore";
-export * from "./createAction";
+export * from './configureStore';
+export * from './createAction';
--- src/modules/something/components/index.ts
+++ src/modules/something/components/index.ts
@@ -1,1 +1,2 @@
 export * from './SomethingList';
+export * from './AddSomething';
